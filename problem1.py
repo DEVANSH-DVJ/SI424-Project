cac0886d554d8fa100598f9e58c365c0c4ef3c3e
devansh-dvj <devansh.dvj@gmail.com>
2021-11-18 17:12:29 +0530

    Updated plot and comments for problem1

--- problem1.py
+++ problem1.py
@@ -13,27 +13,30 @@ def init():
 
     # Population growth rate
     growth_cy_ITA = (population_cy['Total2']['ITA'].diff()[1:] /
-        population_cy['Total2']['ITA'][1:] * 100)
+                     population_cy['Total2']['ITA'][1:] * 100)
     return growth_cy_ITA
+
 
 def estimator(ratelist):
     # The estimate of mu is the mean; of sigma is the standard deviation
-    return (ratelist.mean(), ratelist.std())
+    return (ratelist.mean(), ratelist.std(ddof=0))
+
 
 def run(md, K, N):
-    MeanEstimates = np.zeros((N, 1))
-    StdEstimates = np.zeros((N, 1))
+    mu_estimates = np.zeros((N, 1))
+    sigma_estimates = np.zeros((N, 1))
 
     for i in range(N):
         # Generate growth rate from the underlying true population data
-        ratelist = np.random.choice(md, K, replace = False)
-
+        ratelist = np.random.choice(md, K, replace=False)
         # Save the estimated values
         estimate = estimator(ratelist)
-        MeanEstimates[i] = estimate[0]
-        StdEstimates[i] = estimate[1]
+        mu_estimates[i] = estimate[0]
+        sigma_estimates[i] = estimate[1]
 
-    return [[MeanEstimates.mean(), MeanEstimates.std()], [StdEstimates.mean(), StdEstimates.std()]]
+    # Return average estimated value and std. deviation of estimated values
+    return ((mu_estimates.mean(), mu_estimates.std()),
+            (sigma_estimates.mean(), sigma_estimates.std()))
 
 
 if __name__ == '__main__':
@@ -43,30 +46,29 @@ if __name__ == '__main__':
     # Initialize
     aps = init()
 
-    for N in [10, 100, 1000]:
-        avgs1 = []
-        stds1 = []
-        avgs2 = []
-        stds2 = []
+    for N in [10, 100, 1000, 10000]:
+        mu_avgs = []
+        mu_stds = []
+        sigma_avgs = []
+        sigma_stds = []
 
-        for K in range(20, 141, 1):
+        for K in range(10, 146, 1):
             # Run the experiment for given K, N
             est_info = run(aps, K, N)
-            print('N={}, K={}, p={}'.format(N, K, est_info))
+            print('N={}, K={}, mu={}, sigma={}'.format(N, K, est_info[0], est_info[1]))
 
-            avgs1.append(est_info[0][0])
-            stds1.append(est_info[0][1])
+            mu_avgs.append(est_info[0][0])
+            mu_stds.append(est_info[0][1])
 
-            avgs2.append(est_info[1][0])
-            stds2.append(est_info[1][1])
-
+            sigma_avgs.append(est_info[1][0])
+            sigma_stds.append(est_info[1][1])
 
         # For Estimate of Mu
         # Plot Avg
         plt.clf()
-        plt.plot(range(20, 141, 1), avgs1)
+        plt.plot(range(10, 146, 1), mu_avgs)
         plt.xlabel('K')
-        plt.ylim((0.45, 0.70))
+        plt.ylim((0.46, 0.64))
         plt.ylabel('Average estimated value of mean')
         plt.title('Performed {} experiments for each K'.format(N))
         plt.grid()
@@ -74,21 +76,20 @@ if __name__ == '__main__':
 
         # Plot Std. dev.
         plt.clf()
-        plt.plot(range(20, 141, 1), stds1)
+        plt.plot(range(10, 146, 1), mu_stds)
         plt.xlabel('K')
-        plt.ylim((-0.05, 0.55))
+        plt.ylim((-0.01, 0.24))
         plt.ylabel('Std. deviation of estimated value of mean')
         plt.title('Performed {} experiments for each K'.format(N))
         plt.grid()
         plt.savefig('plots/problem1/Mean/stds_{}.png'.format(N))
 
-
         # For Estimate of Sigma
         # Plot Avg
         plt.clf()
-        plt.plot(range(20, 141, 1), avgs2)
+        plt.plot(range(10, 146, 1), sigma_avgs)
         plt.xlabel('K')
-        plt.ylim((0.2, 0.65))
+        plt.ylim((0.34, 0.71))
         plt.ylabel('Average estimated value of sigma')
         plt.title('Performed {} experiments for each K'.format(N))
         plt.grid()
@@ -96,9 +97,9 @@ if __name__ == '__main__':
 
         # Plot Std. dev.
         plt.clf()
-        plt.plot(range(20, 141, 1), stds2)
+        plt.plot(range(10, 146, 1), sigma_stds)
         plt.xlabel('K')
-        plt.ylim((-0.05, 0.55))
+        plt.ylim((-0.02, 0.46))
         plt.ylabel('Std. deviation of estimated value of sigma')
         plt.title('Performed {} experiments for each K'.format(N))
         plt.grid()
